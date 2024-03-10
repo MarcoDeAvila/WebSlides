f6109e540a8eafa4c2a0a43b152bdc6316487d57
mancl <33145415@uaz.edu.mx>
2024-03-10 01:00:43 -0600

    Agregamos la pagina de incio y separamos la ruta del login, a la vez se crea otro superUsuario

--- webSlides/theone/views.py
+++ webSlides/theone/views.py
@@ -4,4 +4,7 @@ from django.template import Template, Context
 # Create your views here.
 
 def home(request):
+    return HttpResponse("<h1>Pagina de bienvenida</h1>")
+
+def login(request):
     return render(request, 'users/login.html')
