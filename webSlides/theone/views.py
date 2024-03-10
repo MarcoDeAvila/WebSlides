75b7b1d5dd1cb7a8572059a1d3de0f447b77a880
mancl <33145415@uaz.edu.mx>
2024-03-10 12:10:22 -0600

    Agregamos la funcion para registrar el usuario

--- webSlides/theone/views.py
+++ webSlides/theone/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 from django.template import Template, Context
 # Create your views here.
 
@@ -7,6 +9,16 @@ def home(request):
     return HttpResponse("<h1>Pagina de bienvenida</h1>")
 
 def signin(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('user')
+            messages.success(request, 'Hola ' + username + ' tu cuenta ha sido creada exitosamente')
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
     return render(request,'users/signin.html')
 
 def login(request):
