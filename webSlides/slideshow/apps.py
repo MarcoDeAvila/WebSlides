a5224a576662e9f5e6073fab79d3f392771c76ce
FedericoMtzBaez <90460801+FedericoMtzBaez@users.noreply.github.com>
2024-03-17 21:44:47 -0600

    Archivo html creado y css para mostrar las diapositivas

--- webSlides/slideshow/apps.py
+++ webSlides/slideshow/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PresentacionesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'presentaciones'
+    name = 'slideshow'
