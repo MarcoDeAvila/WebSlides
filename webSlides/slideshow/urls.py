98e28177035055240d71aa0775e760438f53d213
Shaixq <e.s.c.q@hotmail.com>
2024-03-19 14:12:22 -0600

    Cambiar el código al estandar pep8 en urls.py

--- webSlides/slideshow/urls.py
+++ webSlides/slideshow/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('slideshow/',views.slideShow, name='slideshow'),
+    path('slideshow/', views.slideShow, name='slideshow'),
     path('createslide/', views.createSlide, name='createslide'),
 ]
 
