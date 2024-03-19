409f95ab28afcbc7d4ba2f14155a957870292c67
Shaixq <e.s.c.q@hotmail.com>
2024-03-19 14:17:06 -0600

    cambiar al estandar PEP8 el urls.py

--- webSlides/theone/urls.py
+++ webSlides/theone/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.signIn, name='signin'), # Inicio sesion
-    path('signup/',views.signUp, name='signup'), # Registrarse
-    path('home/',views.home, name='home'), # Home
-    path('logout/',views.singOut, name='logout'), # Home
+    path('', views.signIn, name='signin'),  # Inicio sesion
+    path('signup/', views.signUp, name='signup'),  # Registrarse
+    path('home/', views.home, name='home'),  # Home
+    path('logout/', views.singOut, name='logout'),  # Home
 ]
-
