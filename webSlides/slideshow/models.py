4679cafd99e5f8d2c5b6d17e7ce6723a8759a06a
Shaixq <e.s.c.q@hotmail.com>
2024-03-19 14:11:12 -0600

    Cambiar el código de models.py

--- webSlides/slideshow/models.py
+++ webSlides/slideshow/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Presentacion(models.Model):
+
     titulo = models.CharField(max_length=200)
     descripcion = models.TextField()
     fecha = models.DateField(auto_now_add=True)
