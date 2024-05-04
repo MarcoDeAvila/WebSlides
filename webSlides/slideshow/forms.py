7d15e9cbf5113c13a61414de2eee24ca7517b3bf
Shaixq <e.s.c.q@hotmail.com>
2024-05-04 15:40:56 -0600

    cambiar forms.py al estandar

--- webSlides/slideshow/forms.py
+++ webSlides/slideshow/forms.py
@@ -5,13 +5,15 @@ class New_Slide_Form(forms.Form):  # inicio sesion
     title = forms.CharField(widget=forms.TextInput(), required=True)
     content = forms.CharField(widget=forms.Textarea(), required=True)
 
+
 class Edit_Slide_Form(forms.Form):  # inicio sesion
     title = forms.CharField(widget=forms.TextInput(attrs={
         'id': 'title'
     }), required=True)
     content = forms.CharField(widget=forms.Textarea(attrs={
         'id': 'content'
-    }), required=True)    
+    }), required=True)
+
 
 class Search_User_Form(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={
